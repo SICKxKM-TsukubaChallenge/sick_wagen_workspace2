18f2619778337d4e536596759a3d0a650469cb1b
curious-jp <curious.ks.jp@gmail.com>
2025-08-05 14:24:59 +0900

    Refactor launch files and remove task_controller package
    - Updated `nakano.launch.py` to include `robot_state_publisher` and adjusted parameters for `nav2_launch`.
    - Corrected the path for the parameters file in `nav2.launch.py`.
    - Deleted the entire `task_controller` package, including its CMakeLists, source files, configuration, and package.xml.

--- sick_wagen/launch/full/nakano.launch.py
+++ sick_wagen/launch/full/nakano.launch.py
@@ -11,27 +11,45 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-
   nav2_launch = IncludeLaunchDescription(
       PythonLaunchDescriptionSource([
           get_package_share_directory('sick_wagen'),
           '/launch/full/nav2.launch.py'
       ]),
       launch_arguments={
-          'namespace': "nav2",
-          'use_sim_time': 'true',
+          # 'namespace': 'nav2',
+          # this makes the bug do not use the namespace
+          'use_sim_time': 'false',
           'autostart': 'true',
           'params_file': os.path.join(
               get_package_share_directory('sick_wagen'),
               'config', 'nav2', 'nav2_params.yaml'
           ),
-          # 'map': os.path.join(
-          #     get_package_share_directory('sick_wagen'),
-          #     'maps', 'nakano_map.yaml'
-          # )
       }.items()
   )
 
+  urdf_file = os.path.join(
+      get_package_share_directory('sick_wagen'),
+      'urdf', 'sick_wagen.urdf'
+  )
+  robot_description = {'robot_description': open(urdf_file).read()}
+  robot_state_publisher = Node(
+      package='robot_state_publisher',
+      executable='robot_state_publisher',
+      name='robot_state_publisher',
+      output='screen',
+      parameters=[
+          robot_description,
+          {'use_sim_time': False}
+      ],
+      remappings=[
+          ('/tf', '/tf'),
+          ('/tf_static', '/tf_static')
+      ]
+  )
+
+
   return LaunchDescription([
-      nav2_launch
+      nav2_launch,
+      robot_state_publisher
   ])
